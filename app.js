a0c345f506010ba0e31d310c3ad4b5bf610ee0b1
DavTheDev20 <reiddavin@outlook.com>
2020-12-17 11:49:54 -0500

    Added authentification to application

--- app.js
+++ app.js
@@ -22,7 +22,42 @@ const User = mongoose.model('User', userSchema);
 
 app.get('/', (req, res) => res.render('home'));
 
-app.get('/register', (req, res) => res.render('register'))
+app.get('/register', (req, res) => res.render('register'));
+
+app.get('/secret', (req, res) => res.render('secret'));
+
+app.post('/login', function(req, res){
+    User.findOne({username: req.body.username}, function(err, user){
+        if (user.password === req.body.password) {
+            res.redirect('/secret')
+        } else {
+           res.redirect('/') ;
+        }
+    });
+
+});
+
+
+app.post('/register', function(req, res){
+
+const username = req.body.username;
+const password = req.body.password;
+
+const newUser = new User({
+    username: req.body.username,
+    password: req.body.password
+});
+
+newUser.save(function(err) {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log('Successfully saved new user to btechDB.');
+        res.redirect('/');
+    }
+});
+
+});
 
 
 app.listen(3000, (req, res) => console.log("Server running on port 3000..."));
